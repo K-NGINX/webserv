265da1ffeb6ba61880247f0751ae290848ec9c86
donghyun1998 <donghyun1998@naver.com>
2023-10-20 23:07:59 +0900

    [add] : ConfigManager에서 kqueue 초기화 부분 추가

--- modules/Server/ServerManager.hpp
+++ modules/Server/ServerManager.hpp
@@ -7,9 +7,13 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <string>
+#include <sys/event.h>
 #include <sys/socket.h> // server.cpp
+#include <sys/time.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <vector>
+
 
 #include "Config/ConfigManager.hpp"
 
@@ -17,10 +21,12 @@ class ServerManager {
 public:
 	static ServerManager& getInstance();
 	void initServer();
-
+	void initKqueue();
+	void closeAllServerFd();
 private:
 	ServerManager();
 	~ServerManager();
-	std::vector<int> v_server_socket_fd_;
+	std::vector<int> v_server_fd_;
+	int kqueue_fd_;
 	// std::vector<int> v_new_socket_fd_; // accept해서 내놓을 소켓
 };
