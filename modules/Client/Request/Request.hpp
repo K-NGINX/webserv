9fc801a1f1d8cfc66dc10781ef045e35a3d2f6f1
donghyun1998 <donghyun1998@naver.com>
2023-10-27 18:14:24 +0900

    [add] : Request parsing class에 껴줌

--- modules/Client/Request/Request.hpp
+++ modules/Client/Request/Request.hpp
@@ -1,10 +1,24 @@
 #pragma once
 
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <unistd.h>
+
+#include "../Utils/Utils.hpp"
+#define BUFFER_SIZE 1024
+#define VERSION "HTTP/1.1"
+#define PLAIN_TEXT "text/plain"
+#define HTML_TEXT "text/html"
+#define JSON_TEXT "application/json"
 enum ParsingRequestStatus {
 	START_LINE,
 	HEADER,
 	BODY,
-	ERROR_PARSING // error면 파싱안하고
+	DONE,
+	ERROR // error면 파싱안하고
 };
 
 class Request {
@@ -12,6 +26,18 @@ public:
     Request();
 
     void parse(int fd);
+	void parseStartLine(std::vector<char>& line);
+	void parseHeader(std::vector<char>& line);
+	void parseBody(std::vector<char>& line);
+	void checkReadSize();
 
     ParsingRequestStatus parsing_status_;
+	std::vector<char> remain_buffer_;
+	std::string method_;
+	std::string uri_;
+	std::string host_;
+	bool empty_line_flag = 0; /////////////////////////////
+	std::map<std::string, std::string> m_header_;
+	std::string body_; // 여기에 body 저장 append로 붙히명 \0 노상관
+	size_t transfer_encoding_chunked_size_ = -1;
 };
