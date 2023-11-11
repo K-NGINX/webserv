b5061dff240f7e8c90842cd2a34d3e69e40eab2c
donghyun kim <donghyun1998@naver.com>
2023-11-11 18:19:39 +0900

    [refactor] : split 로직 엎고 iter find로 바꿔줌

--- modules/Client/Request/Request.hpp
+++ modules/Client/Request/Request.hpp
@@ -2,6 +2,7 @@
 
 #include <unistd.h>
 
+#include <algorithm>
 #include <map>
 #include <sstream>
 #include <string>
@@ -24,36 +25,50 @@ enum RequestStatus {
 	ERROR
 };
 
+enum BodyType {
+	DEFAULT,
+	CHUNKED,
+	BINARY
+};
+
+typedef std::vector<char>::iterator v_c_iter;
+
 class Request {
    public:
 	Request();
-	Request& operator=(const Request& obj);
+	Request &operator=(const Request &obj);
 
 	void parse(int fd);
 	void parseStartLine(std::vector<char> &line);
 	void parseHeader(std::vector<char> &line);
 	void parseBody(std::vector<char> &line);
 	void parseChunkedBody(std::vector<char> &size, std::vector<char> &line);
-	void CheckEmptyRemainBuffer();
+	void checkEmptyRemainBuffer();
+	void refineContentType(std::string &value);
 	void print();
- // gettersetter
+	// gettersetter
 	std::string getConnection() const;
 	RequestStatus getParsing_status() const;
 	std::string getMethod() const;
 	std::string getUri() const;
 	std::string getHost() const;
-	const std::vector<char>& getBody() const;
+	const std::vector<char> &getBody() const;
 	int getBodySize();
+	void func(std::vector<char> &line, v_c_iter &line_start_it, v_c_iter &next_sep_it);
+	void parseBinaryBody(std::vector<char> &line);
+
    private:
+	v_c_iter getNextSepIter(v_c_iter &line_start_it);
 	std::string connection_;
 	RequestStatus parsing_status_;
 	std::string method_;
 	std::string uri_;
 	std::string host_;
-	std::vector<char> remain_buffer_;
-	std::map<std::string, std::string> m_header_; ////////////////////
+	std::vector<char> buffer_;
+	std::map<std::string, std::string> m_header_;	 ////////////////////
 	std::vector<char> body_;
-	int body_size_;
-	bool is_chunked_;
+	size_t body_size_;
+	// bool is_chunked_;///////////////
+	BodyType bodyType_;
 	std::string boundary_;
 };
--- modules/Utils/Utils.cpp
+++ modules/Utils/Utils.cpp
@@ -54,3 +54,26 @@ bool Utils::checkMIMEType(const std::string& mime_type) {
 int Utils::stoi(std::string str) {
 	return atoi(str.c_str());
 }
+/**
+ * @brief v에서 obj를 찾아서 obj의 끝 인덱스를 반환해주는 함수
+ *
+ * @param v base vector
+ * @param obj target vector
+ * @return int 찾은 sub obj의 끝 인덱스를 반환
+ */
+int Utils::findSubVector(std::vector<char>& v, std::vector<char>& obj) {
+	for (size_t i = 0; i < v.size(); ++i) {
+		bool found = true;
+
+		for (size_t j = 0; j < obj.size(); ++j) {
+			if (i + j >= v.size() || v[i + j] != obj[j]) {
+				found = false;
+				break;
+			}
+		}
+		if (found)
+			return static_cast<int>(i + obj.size() - 1);  // obj의 마지막 원소의 인덱스 반환;
+	}
+
+	return -1;	  // obj가 v에 없음
+}
